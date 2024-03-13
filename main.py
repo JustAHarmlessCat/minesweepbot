0289b36d82e19ba1aec5e5b21dcffe8c810b713a
JustAHarmlessCat <sulubjarne@gmail.com>
2024-03-13 17:00:09 +0100

    Update game board initialization and pixel detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyautogui
+import time
 
-img = pyautogui.screenshot(region=(0, 0, 1920, 1080))
+img = pyautogui.screenshot(region=(0, 0, 2560, 1440))
 num1 = 54, 75, 165
 num2 = 47, 147, 82
 num3 = 175, 44, 44
@@ -8,32 +9,45 @@ num4 = 137, 54, 165
 num5 = 110, 58, 58
 num6 = 86, 156, 184
 
+#felder sind 55, 55 pixel groß
 
-def makeBoard(rows, cols):
+def makeBoard():    # 16x30
     board = []
-    for i in range(rows):
+    for i in range(30):
         row = []
-        for j in range(cols):
+        for j in range(16):
             row.append(0)
         board.append(row)
     return board
 
-def whatIsThis(x, y):
-    for i in range (1, 6):
-        if (str(i) + '.png'):                  #hier noch vergleichen pixel mit dem wo das ist
-            return i
-        else: return 0
 
-def updateBoard(board, img):
+def updateBoard(board):
+    width, height = img.size
     for i in range(len(board)):
         for j in range(len(board[i])):
-            if img.getpixel(keine ahnung wie ich hier wat bekomme) !== (255, 255, 255):       #keine ahnung wie ich weiß bei welchem pixel das ist
+            x = 460 + 56 * i
+            y = 257 + 56 * j
+            pixel = img.getpixel((x, y))
+            if pixel != (255, 255, 255):
                 board[i][j] = 0
+            if pixel == num1:
+                board[i][j] = 1
+            if pixel == num2:
+                board[i][j] = 2
+            if pixel == num3:
+                board[i][j] = 3
+            if pixel == num4:
+                board[i][j] = 4
+            if pixel == num5:
+                board[i][j] = 5
+            if pixel == num6:
+                board[i][j] = 6
+    return board
 
-rows = input("Enter the number of rows: ")
-cols = input("Enter the number of columns: ")
+#print('\n'.join(map(str, makeBoard(int(rows), int(cols)))))
 
-print('\n'.join(map(str, makeBoard(int(rows), int(cols)))))
-
-board = makeBoard(int(rows), int(cols))
 pyautogui.click(500, 500)	# Clicks on the screen to start the game
+board = makeBoard()
+time.sleep(1)                   #iwi will das nicht und mann mus das teil zweimal ausführen damit das loppt
+board = updateBoard(board)
+print('\n'.join(map(str, board)))
