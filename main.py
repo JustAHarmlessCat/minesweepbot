df8a1df474d926e8b98e839da77ed14f078b7abf
JustAHarmlessCat <sulubjarne@gmail.com>
2024-03-13 10:51:39 +0100

    Add board creation and update functions

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import pyautogui
 
-x, z = pyautogui.locateCenterOnScreen('1.png')
+img = pyautogui.screenshot(region=(0, 0, 1920, 1080))
+num1 = 54, 75, 165
+num2 = 47, 147, 82
+num3 = 175, 44, 44
+num4 = 137, 54, 165
+num5 = 110, 58, 58
+num6 = 86, 156, 184
 
-print(x, z)
+
+def makeBoard(rows, cols):
+    board = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            row.append(0)
+        board.append(row)
+    return board
+
+def whatIsThis(x, y):
+    for i in range (1, 6):
+        if (str(i) + '.png'):                  #hier noch vergleichen pixel mit dem wo das ist
+            return i
+        else: return 0
+
+def updateBoard(board, img):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if img.getpixel(keine ahnung wie ich hier wat bekomme) !== (255, 255, 255):       #keine ahnung wie ich weiß bei welchem pixel das ist
+                board[i][j] = 0
+
+rows = input("Enter the number of rows: ")
+cols = input("Enter the number of columns: ")
+
+print('\n'.join(map(str, makeBoard(int(rows), int(cols)))))
+
+board = makeBoard(int(rows), int(cols))
+pyautogui.click(500, 500)	# Clicks on the screen to start the game
